2cdf3601933a4ff1d40cc761d4d51c40284ce8d6
Henrik Elsner <helsner@dfau.de>
2017-06-28 13:13:08 +0200

    [TASK] Add NOT

--- Tests/Functional/Parser/QueryParserTest.php
+++ Tests/Functional/Parser/QueryParserTest.php
@@ -508,8 +508,8 @@ class QueryParserTest extends FunctionalTestCase
             'comma value as type string' => ['42,5', 'string', ' ( `title` NOT LIKE \'%42,5\' ) '],
             'string(1 words) as string value as type string' => ['foo', 'string', ' ( `title` NOT LIKE \'%foo\' ) '],
             'string(2 words) as string value as type string' => ['foo bar', 'string', ' ( `title` NOT LIKE \'%foo bar\' ) '],
-            'string(2 words) as string value as type string with %' => ['foo%bar', 'string', ' ( `title` LIKE \'%foo\\\\%bar\' ) '],
-            'string(2 words) as string value as type string with _' => ['foo_bar', 'string', ' ( `title` LIKE \'%foo\\\\_bar\' ) '],
+            'string(2 words) as string value as type string with %' => ['foo%bar', 'string', ' ( `title` NOT LIKE \'%foo\\\\%bar\' ) '],
+            'string(2 words) as string value as type string with _' => ['foo_bar', 'string', ' ( `title` NOT LIKE \'%foo\\\\_bar\' ) '],
         ];
     }
 
