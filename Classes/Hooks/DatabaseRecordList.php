2c383b827d560fd67a1d097050fb3b353bc378ba
Frank Naegler <frank.naegler@typo3.org>
2018-01-30 11:11:33 +0100

    [BUGFIX] Fix broken hook
    Resolves: QUER-1
    Releases: master

--- Classes/Hooks/DatabaseRecordList.php
+++ Classes/Hooks/DatabaseRecordList.php
@@ -3,9 +3,9 @@
 namespace T3G\Querybuilder\Hooks;
 
 use T3G\Querybuilder\Parser\QueryParser;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Recordlist\RecordList\AbstractDatabaseRecordList;
 use TYPO3\CMS\Core\Database\Query\QueryBuilder;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Recordlist\RecordList\DatabaseRecordList as CoreDatabaseRecordList;
 
 /**
  * Class DatabaseRecordList.
@@ -19,20 +19,22 @@ class DatabaseRecordList
      * @param int $pageId the records' page ID
      * @param array $additionalConstraints additional constraints
      * @param array $fieldList field list
-     * @param AbstractDatabaseRecordList $parentObject
+     * @param CoreDatabaseRecordList $parentObject
+     * @param QueryBuilder $queryBuilder
      *
      * @return QueryBuilder
      * @throws \InvalidArgumentException
      */
-    public function buildQueryParametersPostProcess(array &$parameters,
-                                   string $table,
-                                   int $pageId,
-                                   array $additionalConstraints,
-                                   array $fieldList,
-                                   AbstractDatabaseRecordList $parentObject,
-                                   QueryBuilder $queryBuilder) : QueryBuilder
-    {
-        if ($parentObject->table !== null && GeneralUtility::_GP('M') === 'web_list') {
+    public function buildQueryParametersPostProcess(
+        array &$parameters,
+        string $table,
+        int $pageId,
+        array $additionalConstraints,
+        array $fieldList,
+        CoreDatabaseRecordList $parentObject,
+        QueryBuilder $queryBuilder
+    ): QueryBuilder {
+        if ($parentObject->table !== null && GeneralUtility::_GP('route') === '/web/list/') {
             $filter = GeneralUtility::_GP('query');
             if ($filter !== null) {
                 $filter = json_decode($filter);
