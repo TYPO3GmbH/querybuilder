fc25aaf775fb873dc0d3f5c524719ad919d0023b
Henrik Elsner <helsner@dfau.de>
2021-01-16 20:14:19 +0100

    [CLEANUP] Remove sql fields that are provided by TCA

--- ext_tables.sql
+++ ext_tables.sql
@@ -9,8 +9,6 @@ CREATE TABLE sys_querybuilder (
 	queryname varchar(255) DEFAULT '' NOT NULL,
 	where_parts text,
 	user varchar(255) DEFAULT '' NOT NULL,
-	hidden tinyint(1) unsigned DEFAULT '0' NOT NULL,
-	deleted tinyint(1) unsigned DEFAULT '0' NOT NULL,
 
 	PRIMARY KEY (uid)
 );
